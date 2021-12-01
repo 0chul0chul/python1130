36cc62aaa0fab79d4a547d8c5139b2f8728ee327
0chul0chul <0chul0chul@naver.com>
2021-12-01 10:31:39 +0900

    클래스 수업

---
+++ class1.py
@@ -0,0 +1,24 @@
+# class1.py
+
+#1)클래스 형식을 정의
+class Person:
+    #생성자(초기화)메서드
+    def __init__(self):
+        #인스턴스의 멤버 변수를 초기화
+        self.name = "default name"
+    def print(self):
+        print("My name is {0}".format(self.name))
+
+#2)인스턴스 생성
+#디버깅할 때 멈춤(Break Point)
+p1 = Person()
+p1.print()
+
+p2 = Person()
+p2.name = "전우치"
+p2.print()
+
+#런타임(코드가 실행될 떄)변수를 추가할 수 있는
+#동적 형식의 언어
+#디자인 타임
+Person.title = "new title"
